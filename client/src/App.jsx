33bc8473e9f3dd3e6a6e0503500bf10bb05dc7c1
gaurav gautam <gauravgautam9865@gmail.com>
2025-11-09 14:17:02 +0530

    Fix import case for userDataContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/Signup'
 import Customize from './pages/Customize'
-import { userDataContext } from './context/userContext'
+import { userDataContext } from './context/UserContext'
 import Customize2 from './pages/Customize2'
 import Home from './pages/Home'
 
